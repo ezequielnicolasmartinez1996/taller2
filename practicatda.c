a9f6ca4049c14615a692d7cd11abb2ad3c893c26
ezequielnicolasmartinez1996 <ezequielnicolasmartinez1996@gmail.com>
2022-11-11 17:23:28 -0300

    11/11

---
+++ practicatda.c
@@ -0,0 +1,24 @@
+// PRACTICA TDA TALLER 2 AUS
+// EZEQUIEL NICOLAS MARTINEZ
+// DNI: 39755509
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodo
+{
+    char *nombre;
+    struct nodo *sig;
+} Legisladores;
+
+Legisladores *listaLegisladores(Legisladores *lista)
+{
+    lista = NULL;
+    return lista;
+}
+
+int main()
+{
+    Legisladores *lista = listaLegisladores(lista)
+    return 0;
+}
