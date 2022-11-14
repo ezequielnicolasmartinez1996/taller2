de0fdbc947aaf4a3c4df63f62aeade00367271a4
ezequielnicolasmartinez1996 <ezequielnicolasmartinez1996@gmail.com>
2022-11-14 01:35:56 -0300

    14/11

--- practicatda.c
+++ practicatda.c
@@ -4,21 +4,68 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct nodo
+typedef struct legislador
 {
-    char *nombre;
-    struct nodo *sig;
-} Legisladores;
+    char nombre[10];
+    char votacion[15];
+    char presencia[15];
+    char grupo[15];
+} legislador;
 
-Legisladores *listaLegisladores(Legisladores *lista)
+typedef struct nodoLegislador
 {
-    lista = NULL;
-    return lista;
-}
+    legislador p;
+    struct nodoLegislador *siguiente;
+} nodoLegislador;
 
 int main()
 {
-    Legisladores *lista = listaLegisladores(lista)
+    nodoLegislador *inicioLista = NULL;
+    nodoLegislador *primerLegislador = (nodoLegislador *)malloc(sizeof(nodoLegislador));
+
+    strcpy(primerLegislador->p.nombre, "J.D.Peron");
+    strcpy(primerLegislador->p.presencia, "Presente");
+    strcpy(primerLegislador->p.grupo, "ChicosMalos");
+    strcpy(primerLegislador->p.votacion, "Afirmativo");
+    primerLegislador->siguiente = NULL;
+
+    inicioLista = primerLegislador;
+
+    primerLegislador = (nodoLegislador *)malloc(sizeof(nodoLegislador));
+
+    strcpy(primerLegislador->p.nombre, "E.M.Peron");
+    strcpy(primerLegislador->p.presencia, "Presente");
+    strcpy(primerLegislador->p.grupo, "ChicosBuenos");
+    strcpy(primerLegislador->p.votacion, "Negativo");
+    primerLegislador->siguiente = NULL;
+
+    primerLegislador->siguiente = inicioLista;
+    inicioLista = primerLegislador;
+
+    primerLegislador = (nodoLegislador *)malloc(sizeof(nodoLegislador));
+
+    strcpy(primerLegislador->p.nombre, "C.F.K");
+    strcpy(primerLegislador->p.presencia, "Presente");
+    strcpy(primerLegislador->p.grupo, "ChicosMalos");
+    strcpy(primerLegislador->p.votacion, "Afirmativo");
+    primerLegislador->siguiente = NULL;
+
+    primerLegislador->siguiente = inicioLista;
+    inicioLista = primerLegislador;
+    /************************************************************************/
+
+    nodoLegislador *seguidor = inicioLista;
+
+    while (seguidor != NULL)
+    {
+        printf("Nombre: %s\n", seguidor->p.nombre);
+        printf("Presente: %s\n", seguidor->p.presencia);
+        printf("Grupo: %s\n", seguidor->p.grupo);
+        printf("Voto: %s\n", seguidor->p.votacion);
+        seguidor = seguidor->siguiente;
+    }
+    /************************************************************************/
     return 0;
 }
