277983bbafe2f15c2f35e98cc78a833d2516e38b
ezequielnicolasmartinez1996 <ezequielnicolasmartinez1996@gmail.com>
2022-12-13 01:09:22 -0300

    c

--- final1.c
+++ final1.c
@@ -8,14 +8,16 @@
 #define VERTICES 6
 
 void mostrarMatriz(int g[][VERTICES]);
-void calculaSemicamino();
+void calculaSemicamino(int g[][VERTICES]);
 void pesoMinimoSemicamino(int p);
 void igualaMatrices(int g[][VERTICES], int h[][VERTICES]);
 void muestraSemicamino(int g[][1]);
 void actualizaAuxiliar(int g[][VERTICES], int h[][1]);
 
+int camino[VERTICES];
 int n, m, k, i, distanciaCamino = 0;
 int semiCamino[][1];
+int aristaMaxima = 0;
 // matriz [vertice de arista que sale][vertice de arista que entra]
 int matrizCapacidad[VERTICES][VERTICES] = {{0, 4, 6, 0, 0, 0},
                                            {0, 0, 0, 3, 5, 0},
@@ -39,9 +41,10 @@ int aristaPesoMinimo;
 
 int main()
 {
+    mostrarMatriz(matrizAuxiliar);
     igualaMatrices(matrizAuxiliar, matrizCapacidad);
     mostrarMatriz(matrizAuxiliar);
-    calculaSemicamino();
+    calculaSemicamino(matrizCapacidad);
     mostrarMatriz(matrizAuxiliar);
     muestraSemicamino(semiCamino);
     actualizaAuxiliar(matrizAuxiliar, semiCamino);
@@ -61,67 +64,83 @@ void mostrarMatriz(int g[][VERTICES])
     }
 }
 
-void calculaSemicamino()
+void calculaSemicamino(int g[][VERTICES])
 {
-    n = 0;
-    distanciaCamino = 0;
-    int aristaMaxima = 0;
-    int bandera1 = 0;
-    for (int k = 0; k < VERTICES; k++)
-    {
 
-        k = bandera1;
+    aristaPesoMinimo = 100;
+    distanciaCamino = 0;
+    int n = 1;
+    int bandera;
+    int k = 0;
+    camino[0] = 0; // VERTICE SALIENTE
+    while (k < VERTICES)
+    {
+        semiCamino[k][0] = NULL; // limpia la variable
+        semiCamino[k][1] = NULL; // limpia la variable
+
+        k = bandera;
         for (int i = 0; i < VERTICES; i++)
         {
-            if (aristaMaxima < matrizCapacidad[k][i])
+
+            if (aristaMaxima <= g[k][i])
             {
-                aristaMaxima = matrizCapacidad[k][i];
-                bandera1 = i;
+                aristaMaxima = g[k][i];
+                bandera = i;
             }
+            if (aristaMaxima > 0)
+            {
+                pesoMinimoSemicamino(aristaMaxima);
+            }
+            camino[n] = bandera;
         }
-        if (aristaMaxima > 0)
+        n++;
+
+        distanciaCamino++;
+        aristaMaxima = 0;
+        k++;
+    }
+    printf("Camino: ");
+
+    for (int f = 0; f < distanciaCamino; f++)
+    {
+        printf("%d ", camino[f] + 1);
+        if (camino[f] != camino[f + 1])
         {
-            semiCamino[n][0] = k;
-            semiCamino[n][1] = bandera1; // guarda la ruta del semicamino
-            pesoMinimoSemicamino(aristaMaxima);
-            // RESETEAR VARIABLE
-            aristaMaxima = 0;
-            // Distancia del camino
-            distanciaCamino++;
-            n++;
-           // semiCamino[n][1] = NULL; // liberamos este espacio de memoria porque trae error
+            semiCamino[f][0] = camino[f];
+            semiCamino[f][1] = camino[f + 1];
         }
     }
+    printf("\n");
+    printf("Arista de peso minimo: %d\n", aristaPesoMinimo);
 }
 
 void muestraSemicamino(int g[][1])
 {
     printf("El semicamino es:\n");
-    for (int i = 0; i < distanciaCamino; i++)
+    for (int i = 0; i < distanciaCamino - 1; i++)
     {
         for (int k = 0; k < 2; k++)
         {
             printf("%d ", g[i][k] + 1);
         }
-        printf("- El peso de la arista es: %d", aristaPesoMinimo);
+        printf("- El peso minimo de la arista es: %d", aristaPesoMinimo);
         printf("\n");
     }
 }
 
 void actualizaAuxiliar(int g[][VERTICES], int h[][1])
 {
-    for (int i = 0; i < distanciaCamino; i++)
+    for (int i = 0; i < distanciaCamino - 1; i++)
     {
         int coordx = h[i][0];
         int coordy = h[i][1];
         printf("Coordx: %d, coordy;%d, semicamino:%d, semicamino:%d\n", coordx, coordy, h[i][0], h[i][1]);
-        g[coordx][coordx]= g[coordx][coordy] - aristaPesoMinimo;
+        g[coordx][coordy] = g[coordx][coordy] - aristaPesoMinimo;
     }
 }
 
 void pesoMinimoSemicamino(int p)
 {
-    aristaPesoMinimo = p;
     if (p < aristaPesoMinimo)
         aristaPesoMinimo = p;
 }
